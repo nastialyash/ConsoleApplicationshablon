2dcdd43fe78207cfdbaf9dbd114ab7b72a4ae406
nastialyash <user251@school22.rv.ua>
2024-09-25 21:26:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplicationshablon.cpp
@@ -0,0 +1,79 @@
+﻿
+#include <iostream>
+using namespace std;
+
+    
+   /* template <typename T>
+    void swapValues(T & a, T & b) {
+        T temp = a;
+        a = b;
+        b = temp;
+    }*/
+ /* template <typename T>
+        T arraySum(T arr[], int size) {
+            T sum = 0;
+            for (int i = 0; i < size; ++i) {
+                sum += arr[i];
+            }
+            return sum;
+        }*/
+  /*template <typename T>
+        int countElements(T arr[], int size) {
+            return size;
+        }*/
+
+    int main() {
+
+
+
+        
+        /*int int1 = 5, int2 = 10;
+        cout << " Before" << int1 << " int2 = " << int2 << endl;
+        swapValues(int1, int2);
+        cout << "After" << int1 << " int2 = " << int2 << endl;
+
+        
+        double double1 = 3.14, double2 = 2.71;
+        cout << "Before " << double1 << " double2 = " << double2 << endl;
+        swapValues(double1, double2);
+        cout << " After" << double1 << " double2 = " << double2 << endl;*/
+
+      
+
+      /*
+            int intArray[] = { 1, 2, 3, 4, 5 };
+            int intSize = sizeof(intArray) / sizeof(intArray[0]);
+            cout << "Sum of nums : " << arraySum(intArray, intSize) << endl;
+
+            
+            double doublearr[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            int doubleSize = sizeof(doublearr) / sizeof(doublearr[0]);
+            cout << "Sum of double : " << arraySum(doublearr, doubleSize) << endl;
+
+           
+            float floatarr[] = {  2.5, 3.5, 4.5, 5.5 };
+            int floatSize = sizeof(floatarr) / sizeof(floatarr[0]);
+            cout << "Sum of float : " << arraySum(floatarr, floatSize) << endl;
+*/
+      
+
+    /*
+           
+            int arr[] = { 1, 2, 3, 4, 5 };
+            int Size = sizeof(arr) / sizeof(arr[0]);
+            cout << "Cout of elements: " << countElements(arr, Size) << endl;
+
+         
+            double arr2[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            int doubleSize = sizeof(arr2) / sizeof(arr2[0]);
+            cout << "Cout of double : " << countElements(arr2, doubleSize) << endl;
+
+            
+            float arr3[] = { 1.5, 2.5, 3.5, 4.5, 5.5 };
+            int floatSize = sizeof(arr3) / sizeof(arr3[0]);
+            cout << "Cout of float :  " << countElements(arr3, floatSize) << endl;
+*/
+
+
+}
+
